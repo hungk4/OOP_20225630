1ca8532eb3a042a45f5ec455aba2170c84d09d29
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-09-30 12:35:50 +0700

    lab 01

---
+++ Lab01/code/HelloNameDialog.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class HelloNameDialog {
+  public static void main(String[] args) {
+    String result1;
+    result1 = JOptionPane.showInputDialog("Nhap ten cua ban");
+    JOptionPane.showMessageDialog(null, "hi " + result1 + "!");
+  }
+}
